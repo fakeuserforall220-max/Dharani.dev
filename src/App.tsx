2923bf88fa33ce6f471c0eb1436e5dee68267123
fakeuserforall220-max <fakeuserforall220@gmail.com>
2026-08-21 11:49:03 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,66 +1,18 @@
-import { useEffect } from "react";
-import Lenis from "lenis";
-import { motion, useScroll, useSpring } from "framer-motion";
-import { Navbar } from "./components/Navbar";
-import { Hero } from "./components/Hero";
-import { About } from "./components/About";
-import { Projects } from "./components/Projects";
-import { Skills } from "./components/Skills";
-import { Highlights } from "./components/Highlights";
-import { Contact } from "./components/Contact";
-import { Footer } from "./components/Footer";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { AuthProvider } from "./lib/AuthContext";
+import { PortfolioSite } from "./PortfolioSite";
+import { AdminGate } from "./admin/AdminGate";
+import { ADMIN_PATH } from "./lib/adminPath";
 
 export default function App() {
-  useEffect(() => {
-    const lenis = new Lenis({
-      duration: 1.2,
-      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
-      orientation: "vertical",
-      gestureOrientation: "vertical",
-      smoothWheel: true,
-      wheelMultiplier: 1,
-      touchMultiplier: 2,
-    });
-
-    function raf(time: number) {
-      lenis.raf(time);
-      requestAnimationFrame(raf);
-    }
-
-    requestAnimationFrame(raf);
-
-    return () => {
-      lenis.destroy();
-    };
-  }, []);
-
-  const { scrollYProgress } = useScroll();
-  const scaleX = useSpring(scrollYProgress, {
-    stiffness: 100,
-    damping: 30,
-    restDelta: 0.001,
-  });
-
   return (
-    <div className="bg-brand-black min-h-screen text-white font-sans selection:bg-brand-purple/30 selection:text-brand-purple">
-      {/* Scroll Progress Bar */}
-      <motion.div
-        className="fixed top-0 left-0 right-0 h-1 bg-gradient-to-r from-brand-violet via-brand-purple to-brand-blue origin-left z-[100]"
-        style={{ scaleX }}
-      />
-      
-      <Navbar />
-      
-      <main>
-        <Hero />
-        <About />
-        <Highlights />
-        <Projects />
-        <Skills />
-        <Contact />
-      </main>
-
-      <Footer />
-    </div>
+    <BrowserRouter>
+      <AuthProvider>
+        <Routes>
+          <Route path="/" element={<PortfolioSite />} />
+          <Route path={ADMIN_PATH} element={<AdminGate />} />
+        </Routes>
+      </AuthProvider>
+    </BrowserRouter>
   );
 }
